23301d275dd5481bd46d02e5827b426b47bb5ae1
dmirty.s <d.strizhakov@gmail.com>
2024-02-17 17:35:51 +0300

    feat: add simple ws controller

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
+import { WebSocketController } from "./src/ws_server/WebSocketController";
 import { httpServer } from "./src/http_server/index";
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = process.env.HTTP_PORT || 8181;
+const WSS_PORT = Number(process.env.WSS_PORT || 3000);
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`Start static http server on the ${HTTP_PORT} port!`);
+});
+
+new WebSocketController(WSS_PORT);
+
+
